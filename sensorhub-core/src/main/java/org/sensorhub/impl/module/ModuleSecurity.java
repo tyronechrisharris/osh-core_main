sahand-kh/osh-core

Removed SensorHub and AdminUI singletons

sensorhub-core/src/main/java/org/sensorhub/impl/module/ModuleSecurity.java
@@ -18,20 +18,21 @@ import java.security.AccessControlException;
 import org.sensorhub.api.module.IModule;
 import org.sensorhub.api.security.IPermission;
 import org.sensorhub.api.security.IUserInfo;
-import org.sensorhub.impl.SensorHub;
 import org.sensorhub.impl.security.ModulePermissions;
 import org.sensorhub.impl.security.PermissionRequest;
 
 
 public class ModuleSecurity
 {    
+    final IModule<?> module;
     public final ModulePermissions rootPerm;
     protected boolean enable = true;
-    ThreadLocal<IUserInfo> currentUser = new ThreadLocal<IUserInfo>();
+    ThreadLocal<IUserInfo> currentUser = new ThreadLocal<>();
         
     
     public ModuleSecurity(IModule<?> module, String moduleTypeAlias, boolean enable)
     {
+        this.module = module;
         this.rootPerm = new ModulePermissions(module, moduleTypeAlias);
         this.enable = enable;
         
@@ -47,7 +48,7 @@ public class ModuleSecurity
     
     private final boolean isAccessControlEnabled()
     {
-        return SensorHub.getInstance().getSecurityManager().isAccessControlEnabled();
+        return module.getParentHub().getSecurityManager().isAccessControlEnabled();
     }
     
     
@@ -67,7 +68,7 @@ public class ModuleSecurity
             throw new SecurityException(perm.getErrorMessage() + ": No user specified");
         
         // request authorization
-        return SensorHub.getInstance().getSecurityManager().isAuthorized(user, new PermissionRequest(perm));
+        return module.getParentHub().getSecurityManager().isAuthorized(user, new PermissionRequest(perm));
     }
     
     
@@ -104,7 +105,7 @@ public class ModuleSecurity
             return;
         
         // lookup user info 
-        IUserInfo user = SensorHub.getInstance().getSecurityManager().getUserInfo(userID);
+        IUserInfo user = module.getParentHub().getSecurityManager().getUserInfo(userID);
         if (user == null)
             throw new SecurityException("Permission denied: Unknown user " + userID);
         
@@ -118,5 +119,14 @@ public class ModuleSecurity
     public void clearCurrentUser()
     {
         currentUser.remove();
+    }
+    
+    
+    /**
+     * Call to register module permissions on the sensor hub
+     */
+    protected void register()
+    {
+        module.getParentHub().getSecurityManager().registerModulePermissions(rootPerm);
     }
 }

sensorhub-service-swe/src/main/java/org/sensorhub/impl/service/sos/SOSSecurity.java
@@ -15,7 +15,6 @@ Copyright (C) 2012-2016 Sensia Software LLC. All Rights Reserved.
 package org.sensorhub.impl.service.sos;
 
 import org.sensorhub.api.security.IPermission;
-import org.sensorhub.impl.SensorHub;
 import org.sensorhub.impl.module.ModuleSecurity;
 import org.sensorhub.impl.security.ItemPermission;
 import org.sensorhub.impl.security.ModulePermissions;
@@ -71,14 +70,14 @@ public class SOSSecurity extends ModuleSecurity
         // register wildcard permission tree usable for all SOS services
         // do it at this point so we don't include specific offering permissions
         ModulePermissions templatePerm = rootPerm.cloneAsTemplatePermission("SOS Services");
-        SensorHub.getInstance().getSecurityManager().registerModulePermissions(templatePerm);
+        sos.getParentHub().getSecurityManager().registerModulePermissions(templatePerm);
                 
         // create permissions for each offering
         for (SOSProviderConfig offering: sos.getConfiguration().dataProviders)
             addOfferingPermissions(offering.offeringID);
         
-        // register this instance permission tree
-        SensorHub.getInstance().getSecurityManager().registerModulePermissions(rootPerm);
+        // register permission tree
+        register();
     }
     
     

tyronechrisharris/osh-core_main

sensorhub-core/src/main/java/org/sensorhub/impl/module/ModuleSecurity.java
@@ -25,6 +25,7 @@ import org.sensorhub.impl.security.PermissionRequest;
 
 public class ModuleSecurity
 {    
+    final IModule<?> module;
     public final ModulePermissions rootPerm;
     protected boolean enable = true;
     ThreadLocal<IUserInfo> currentUser = new ThreadLocal<>();
@@ -32,6 +33,7 @@ public class ModuleSecurity
     
     public ModuleSecurity(IModule<?> module, String moduleTypeAlias, boolean enable)
     {
+        this.module = module;
         this.rootPerm = new ModulePermissions(module, moduleTypeAlias);
         this.enable = enable;
         
@@ -47,7 +49,7 @@ public class ModuleSecurity
     
     private final boolean isAccessControlEnabled()
     {
-        return SensorHub.getInstance().getSecurityManager().isAccessControlEnabled();
+        return module.getParentHub().getSecurityManager().isAccessControlEnabled();
     }
     
     
@@ -67,7 +69,7 @@ public class ModuleSecurity
             throw new AccessControlException(perm.getErrorMessage() + ": No user specified");
         
         // request authorization
-        return SensorHub.getInstance().getSecurityManager().isAuthorized(user, new PermissionRequest(perm));
+        return module.getParentHub().getSecurityManager().isAuthorized(user, new PermissionRequest(perm));
     }
     
     
@@ -104,7 +106,7 @@ public class ModuleSecurity
             return;
         
         // lookup user info 
-        IUserInfo user = SensorHub.getInstance().getSecurityManager().getUserInfo(userID);
+        IUserInfo user = module.getParentHub().getSecurityManager().getUserInfo(userID);
         if (user == null)
             throw new AccessControlException("Permission denied: Unknown user " + userID);
         
@@ -126,6 +128,6 @@ public class ModuleSecurity
      */
     public void unregister()
     {
-        SensorHub.getInstance().getSecurityManager().unregisterModulePermissions(rootPerm);
+        module.getParentHub().getSecurityManager().unregisterModulePermissions(rootPerm);
     }
 }

sensorhub-service-swe/src/main/java/org/sensorhub/impl/service/sos/SOSSecurity.java
@@ -15,7 +15,6 @@ Copyright (C) 2012-2016 Sensia Software LLC. All Rights Reserved.
 package org.sensorhub.impl.service.sos;
 
 import org.sensorhub.api.security.IPermission;
-import org.sensorhub.impl.SensorHub;
 import org.sensorhub.impl.module.ModuleSecurity;
 import org.sensorhub.impl.security.ItemPermission;
 import org.sensorhub.impl.security.ModulePermissions;
@@ -71,14 +70,14 @@ public class SOSSecurity extends ModuleSecurity
         // register wildcard permission tree usable for all SOS services
         // do it at this point so we don't include specific offering permissions
         ModulePermissions wildcardPerm = rootPerm.cloneAsTemplatePermission("SOS Services");
-        SensorHub.getInstance().getSecurityManager().registerModulePermissions(wildcardPerm);
+        sos.getParentHub().getSecurityManager().registerModulePermissions(wildcardPerm);
                 
         // create permissions for each offering
         for (SOSProviderConfig offering: sos.getConfiguration().dataProviders)
             addOfferingPermissions(offering.offeringID);
         
-        // register this instance permission tree
-        SensorHub.getInstance().getSecurityManager().registerModulePermissions(rootPerm);
+        // register permission tree
+        register();
     }
     
     