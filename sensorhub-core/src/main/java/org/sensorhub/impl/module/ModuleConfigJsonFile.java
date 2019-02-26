9299c89bdd06cc8b2dbde0eea2274fec3efb6655
Alex Robin <alex.robin@sensiasoft.com>
2019-02-26 08:36:27 +0100

    Allow serialization of NaN and ISO8601 dates in JSON config file

--- sensorhub-core/src/main/java/org/sensorhub/impl/module/ModuleConfigJsonFile.java
+++ sensorhub-core/src/main/java/org/sensorhub/impl/module/ModuleConfigJsonFile.java
@@ -41,6 +41,8 @@ import com.google.gson.JsonPrimitive;
 import com.google.gson.TypeAdapter;
 import com.google.gson.TypeAdapterFactory;
 import com.google.gson.internal.Streams;
+import com.google.gson.internal.bind.JsonTreeReader;
+import com.google.gson.internal.bind.JsonTreeWriter;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
@@ -93,7 +95,6 @@ public class ModuleConfigJsonFile implements IModuleConfigRepository
             
             return new TypeAdapter<R>()
             {
-                @SuppressWarnings("unchecked")
                 @Override
                 public R read(JsonReader in) throws IOException
                 {
@@ -120,18 +121,25 @@ public class ModuleConfigJsonFile implements IModuleConfigRepository
                         }
                     }
                     
-                    return delegate.fromJsonTree(jsonElement);
+                    JsonReader jsonReader = new JsonTreeReader(jsonElement);
+                    jsonReader.setLenient(true);
+                    return delegate.read(jsonReader);
                 }
 
 
-                @SuppressWarnings("unchecked")
                 @Override
                 public void write(JsonWriter out, R value) throws IOException
                 {
                     Class<R> runtimeClass = (Class<R>)value.getClass();
                     String typeName = runtimeClass.getName();
                     TypeAdapter<R> delegate = gson.getDelegateAdapter(RuntimeTypeAdapterFactory.this, TypeToken.get(runtimeClass));
-                    JsonElement jsonElt = delegate.toJsonTree(value);
+                    
+                    //JsonElement jsonElt = delegate.toJsonTree(value); // JsonTreeWriter is not lenient in this case                    
+                    JsonTreeWriter jsonWriter = new JsonTreeWriter();
+                    jsonWriter.setLenient(true);
+                    jsonWriter.setSerializeNulls(false);
+                    delegate.write(jsonWriter, value);
+                    JsonElement jsonElt = jsonWriter.get();
                     
                     if (jsonElt.isJsonObject())
                     {
@@ -164,8 +172,11 @@ public class ModuleConfigJsonFile implements IModuleConfigRepository
         
         // init json serializer/deserializer
         final GsonBuilder builder = new GsonBuilder();
+        builder.setLenient();
         builder.setPrettyPrinting();
         builder.disableHtmlEscaping();
+        builder.serializeSpecialFloatingPointValues();
+        builder.setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
         builder.registerTypeAdapterFactory(new RuntimeTypeAdapterFactory<Object>(Object.class, OBJ_CLASS_FIELD));
         
         gson = builder.create();
