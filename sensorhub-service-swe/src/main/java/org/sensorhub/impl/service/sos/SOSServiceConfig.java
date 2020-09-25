f11e5dfe7ccc5e7500032f0aa97ebe8a0f6a5834
Alex Robin <alex.robin@sensiasoft.com>
2020-09-25 19:56:54 +0200

    Added default live timeout in SOS config

--- sensorhub-service-swe/src/main/java/org/sensorhub/impl/service/sos/SOSServiceConfig.java
+++ sensorhub-service-swe/src/main/java/org/sensorhub/impl/service/sos/SOSServiceConfig.java
@@ -7,18 +7,22 @@ at http://mozilla.org/MPL/2.0/.
 Software distributed under the License is distributed on an "AS IS" basis,
 WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
 for the specific language governing rights and limitations under the License.
- 
+
 Copyright (C) 2012-2015 Sensia Software LLC. All Rights Reserved.
- 
+
 ******************************* END LICENSE BLOCK ***************************/
 
 package org.sensorhub.impl.service.sos;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 import org.sensorhub.api.config.DisplayInfo;
-import org.sensorhub.api.persistence.StorageConfig;
+import org.sensorhub.api.config.DisplayInfo.FieldType;
+import org.sensorhub.api.config.DisplayInfo.FieldType.Type;
+import org.sensorhub.api.config.DisplayInfo.Required;
 import org.sensorhub.api.security.SecurityConfig;
+import org.sensorhub.impl.sensor.VirtualProcedureGroupConfig;
 import org.sensorhub.impl.service.ogc.OGCServiceConfig;
 
 
@@ -32,44 +36,72 @@ import org.sensorhub.impl.service.ogc.OGCServiceConfig;
  */
 public class SOSServiceConfig extends OGCServiceConfig
 {
-    
+
+    @DisplayInfo(label="Database ID", desc="ID of database module used for persisting data received by this service."
+        + "If none is provided, new procedures are still registered on the hub but observations are only pushed to the event bus, "
+        + "without any persistence guarantee.")
+    public String databaseID = null;
+
+
+    @Required
+    @DisplayInfo(desc="Metadata of procedure group that will be created to contain all sensors "
+        + "registered through this service. Only sensors in this group will be modifiable by this service")
+    public VirtualProcedureGroupConfig virtualSensorGroup = null;
+
+
+    @FieldType(Type.PROCEDURE_UID)
+    @DisplayInfo(desc="Unique IDs of procedures to expose through this service. "
+        + "Each item in the list can also be a prefix with a trailing wildcard to match multiple procedures.")
+    public LinkedHashSet<String> exposedProcedures = new LinkedHashSet<>();
+
+
+    @FieldType(Type.PROCEDURE_UID)
+    @DisplayInfo(desc="Unique IDs of procedures to exclude. "
+        + "Each item in the list can also be a prefix with a trailing wildcard to match multiple procedures.")
+    public LinkedHashSet<String> excludedProcedures = null;
+
+
     @DisplayInfo(desc="Set to true to enable transactional operation support")
     public boolean enableTransactional = false;
-    
-    
-    @DisplayInfo(label="Max Observations Returned", desc="Maximum number of observations returned by a historical GetObservation request (for each selected offering)")
+
+
+    @DisplayInfo(label="Max Observations Returned", desc="Maximum number of observations returned "
+        + " by a historical GetObservation request (for each selected offering)")
     public int maxObsCount = 100;
-    
-    
+
+
     @DisplayInfo(label="Max Records Returned", desc="Maximum number of result records returned by a historical GetResult request")
     public int maxRecordCount = 100000;
+
+
+    @DisplayInfo(desc="Maximum number of FoI IDs listed in capabilities")
+    public int maxFois = 10;
     
     
-    @DisplayInfo(desc="Storage configuration to use for newly registered sensors")
-    public StorageConfig newStorageConfig;
-    
-    
-    @DisplayInfo(label="Offerings", desc="Configuration of data providers for SOS offerings")
-    public List<SOSProviderConfig> dataProviders = new ArrayList<>();
-    
-    
-    @DisplayInfo(desc="Configuration of data consumers for SOS offerings created by SOS-T")
-    public List<SOSConsumerConfig> dataConsumers = new ArrayList<>();
-    
-    
+    @DisplayInfo(desc="Default live time-out for new offerings created via SOS-T")
+    public double defaultLiveTimeout = 600.0;
+
+
+    @DisplayInfo(desc="Time-out period after which a template ID reserved using InsertResultTemplate "
+        + "will expire if not used in InsertResult requests (in seconds)")
+    public int templateTimeout = 600;
+
+
+    @DisplayInfo(desc="Custom provider configurations. ")
+    public LinkedHashSet<SOSProviderConfig> providerConfigurations = new LinkedHashSet<>();
+
+
     @DisplayInfo(desc="Mapping of custom formats mime-types to custom serializer classes")
     public List<SOSCustomFormatConfig> customFormats = new ArrayList<>();
-    
-    
+
+
     @DisplayInfo(desc="Security related options")
     public SecurityConfig security = new SecurityConfig();
-    
-    
+
+
     public SOSServiceConfig()
     {
         this.moduleClass = SOSService.class.getCanonicalName();
         this.endPoint = "/sos";
-        this.customFormats.add(new SOSCustomFormatConfig("video/mp4", "org.sensorhub.impl.service.sos.video.MP4Serializer"));
-        this.customFormats.add(new SOSCustomFormatConfig("video/x-motion-jpeg", "org.sensorhub.impl.service.sos.video.MJPEGSerializer"));
     }
 }
