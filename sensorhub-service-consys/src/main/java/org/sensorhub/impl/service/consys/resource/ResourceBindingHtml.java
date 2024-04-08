5d93f02da5871b8efa6297129670ffb219436354
Nick Garay <nicolasgaray@icloud.com>
2024-04-08 14:16:40 -0500

    Load Connected Systems API css and js from jar file rather than pulli… (#257)
    * Load Connected Systems API css and js from jar file rather than pulling from CDN.
    
    * Fix issues with UnterminatedStringLiteralException

--- sensorhub-service-consys/src/main/java/org/sensorhub/impl/service/consys/resource/ResourceBindingHtml.java
+++ sensorhub-service-consys/src/main/java/org/sensorhub/impl/service/consys/resource/ResourceBindingHtml.java
@@ -98,25 +98,18 @@ public abstract class ResourceBindingHtml<K, V> extends ResourceBinding<K, V>
         
         // head
         head(
-            title("OpenSensorHub - Connected Systems API"),
-            meta().withCharset("UTF-8"),
-            link()
-                .withRel("stylesheet")
-                .withHref("https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css")
-                .attr("integrity", "sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3")
-                .attr("crossorigin", ""),
-            link()
-                .withRel("stylesheet")
-                .withHref("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css"),
-            script()
-                .withSrc("https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js")
-                .attr("integrity", "sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p")
-                .attr("crossorigin", ""),
-            style()
-                .withText(
-                    ".accordion-button { background-color: #e7f1ff; }"
-                ),
-            getExtraHeaderContent()
+                title("OpenSensorHub - Connected Systems API"),
+                meta().withCharset("UTF-8"),
+                styleWithInlineFile("/css/bootstrap.min.css")
+                        .attr("integrity", "sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"),
+                styleWithInlineFile("/css/bootstrap-icons.css"),
+                scriptWithInlineFile_min("/js/bootstrap.bundle.min.js")
+                        .attr("integrity", "sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"),
+                style()
+                        .withText(
+                                ".accordion-button { background-color: #e7f1ff; }"
+                        ),
+                getExtraHeaderContent()
         )
         .render(html);
         
