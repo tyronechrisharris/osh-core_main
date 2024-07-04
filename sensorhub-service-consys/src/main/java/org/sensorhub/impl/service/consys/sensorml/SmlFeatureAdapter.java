73475990f3cd45401f75f499a9a3eeb54006a714
Alex Robin <alex.robin@sensiasoft.com>
2024-07-04 16:04:41 +0200

    [CSAPI] Fix logic to get geometry from SML features. Fixes #253

--- sensorhub-service-consys/src/main/java/org/sensorhub/impl/service/consys/sensorml/SmlFeatureAdapter.java
+++ sensorhub-service-consys/src/main/java/org/sensorhub/impl/service/consys/sensorml/SmlFeatureAdapter.java
@@ -93,7 +93,7 @@ public abstract class SmlFeatureAdapter<T extends DescribedObject> implements IS
     public AbstractGeometry getGeometry()
     {
         var sml = getFullDescription();
-        return sml != delegate.getGeometry() ? sml.getGeometry() : null;
+        return sml != null ? sml.getGeometry() : delegate.getGeometry();
     }
     
 
